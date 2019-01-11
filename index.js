c90fa73bf79d810d7bb56c7b6218c16e831365fb
hubaojun <hubaojun@021.com>
2019-01-11 11:10:02 +0800

    1.增加loose模式\n2.增加rootDir配置项\n3.增加outputPath配置项

--- index.js
+++ index.js
@@ -11,10 +11,15 @@ var FILE_DECL = /(?:href=|src=|url\()['|"]([^\s>"']+?)\?rev=([^\s>"']+?)['|"]/gi
 
 var revPlugin = function revPlugin(config) {
   config = config||{};
-  var {mode = 'strict'} = config;
+  var {
+    mode = 'strict',
+    outputPath,
+    rootDir = ''
+  } = config;
+  var rootPath = path.join(__dirname,'../../'+rootDir);
+  var outputRootPath = path.join(__dirname,'../../');
 
   return map(function(file, cb) {
-
     var contents;
     var lines;
     var i, length;
@@ -33,37 +38,53 @@ var revPlugin = function revPlugin(config) {
     }
 
     contents = file.contents.toString();
+
     lines = contents.split('\n');
+
     length = lines.length;
 
     for(i = 0; i < length; i++) {
       line = lines[i];
       declarations = line.match(FILE_DECL);
+
       if (declarations && declarations.length > 0) {
+
         for(var j = 0; j < declarations.length; j++) {
+
           groups = FILE_DECL.exec(declarations[j]);
+
           if(groups && groups.length > 1) {
             // are we an "absoulte path"? (e.g. /js/app.js)
-            var normPath = path.normalize(groups[1]);
-            if (normPath.indexOf(path.sep) === 0) {
-              dependencyPath = path.join(file.base, normPath);
-            }
-            else {
-              dependencyPath = path.resolve(path.dirname(file.path), normPath);
-            }
-
-            switch (mode) { 
+            switch (mode) {
               case 'easy':
                 line = line.replace(groups[2], new Date().getTime());
                 break;
               default:
+                var normPath = path.normalize(groups[1]);
+                if(/^(http:\\|https:\\|\\\\)/.test(normPath)){ // cdn 链接
+                  dependencyPath = normPath
+                } else if (normPath.indexOf(path.sep) === 0) { // 绝对路径
+                  dependencyPath = path.join(rootPath,normPath);
+                } else { // 其他 相对路径
+                  if(outputPath){ // 配置了输出路径
+                    console.log(outputRootPath,outputPath,normPath);
+                    dependencyPath = path.resolve(outputRootPath, outputPath ,normPath);
+                  }else{
+                    dependencyPath = path.resolve(path.dirname(file.path), normPath);
+                  }
+                }
                 try {
                   data = fs.readFileSync(dependencyPath);
                   hash = crypto.createHash('md5');
                   hash.update(data.toString(), 'utf8');
                   line = line.replace(groups[2], hash.digest('hex'));
                 } catch(e) {
-                  console.log('The file could not be found.[path:'+dependencyPath+']')
+                  // if(mode==='loose'){
+                  //   line = line.replace(groups[2], new Date().getTime());
+                  // }else{
+                  console.log('The file could not be found ---- [path:'+dependencyPath+']')
+                  // }
+
                 }
                 break;
             }
